e6c68de7ca1c4bdbae3999ad21c7415648294e63
sajal <sajalsahu8@gmail.com>
2024-04-15 12:25:44 +0530

    updataed

--- client/listfiles.py
+++ client/listfiles.py
@@ -12,7 +12,7 @@ print('connected')
 def files_on_server(conn):
    
     print("Requesting files...\n")
-
+    file_dict = {}
     try:
         # First get the number of files in the directory
         number_of_files = struct.unpack("i", conn.recv(4))[0]
@@ -20,20 +20,27 @@ def files_on_server(conn):
         # Then enter into a loop to receive details of each, one by one
         for i in range(int(number_of_files)):
             # Get the file name size first to slightly lessen amount transferred over socket
+           
             file_name_size = struct.unpack("i", conn.recv(4))[0]
             conn.send("351".encode())
             print(f"file_name_size {file_name_size}")
+           
             file_name = conn.recv(file_name_size).decode()
             conn.send("352".encode())
-            print(f"file_name {file_name}")
+            
             # Also get the file size for each item in the server
             file_size = struct.unpack("i", conn.recv(4))[0]
             conn.send("353".encode())
             print(f"file_size {file_size}")
-            print("\t{} - {}b".format(file_name, file_size))
+             
+            file_dict[file_name] = file_size
+             
             # Make sure that the client and server are synchronized
             conn.send("200".encode())
         # Get total size of directory
+        for file_name, file_size in file_dict.items():
+            print(f"{file_name} | {file_size} bytes")
+
         total_directory_size = struct.unpack("i", conn.recv(4))[0]  
         conn.send("355".encode())
         print("Total directory size: {}b".format(total_directory_size))
